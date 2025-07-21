dea1a402f6c20462a065c2043974804908e2933c
Щепетин Илья <shchepetin.i@dornet.ru>
2025-07-21 12:47:57 +0300

    HW16_v4

--- src/test/java/utils/CustomAllureListener.java
+++ src/test/java/utils/CustomAllureListener.java
@@ -4,8 +4,7 @@ import io.qameta.allure.restassured.AllureRestAssured;
 
 public class CustomAllureListener extends AllureRestAssured {
     public CustomAllureListener() {
-        super()
-                .setRequestTemplate("allure-templates/request.ftl")
-                .setResponseTemplate("allure-templates/response.ftl");
+        setRequestTemplate("allure-templates/request.ftl");
+        setResponseTemplate("allure-templates/response.ftl");
     }
 }
